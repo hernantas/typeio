dcbde16507681108872f72ef4168277e55812432
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-12-23 15:25:32 +0700

    Add `TupleType`

---
+++ packages/type-io-type/src/alias/TupleType.ts
@@ -0,0 +1,1 @@
+export type TupleType = [unknown, ...unknown[]]
--- packages/type-io-type/src/alias/index.ts
+++ packages/type-io-type/src/alias/index.ts
@@ -1,3 +1,4 @@
 export * from './ConstructorType'
 export * from './LiteralType'
 export * from './ObjectType'
+export * from './TupleType'
