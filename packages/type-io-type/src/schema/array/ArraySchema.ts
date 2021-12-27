9030a01816a4d58a47e4b9b45d5d581681d285ec
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-12-27 14:30:57 +0700

    Add `parse` to `ArraySchema`

--- packages/type-io-type/src/schema/array/ArraySchema.ts
+++ packages/type-io-type/src/schema/array/ArraySchema.ts
@@ -9,4 +9,11 @@ export class ArraySchema<T extends AnySchema> extends BaseSchema<TypeOf<T>, Arra
   get type (): T {
     return this.definition.type
   }
+
+  parse (input: unknown): TypeOf<T> {
+    if (!Array.isArray(input)) {
+      throw new Error('Input type must be an array')
+    }
+    return input
+  }
 }
