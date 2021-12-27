7686d8106d46a7a3cc19adf659a43cdc36b49adc
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-12-27 15:53:05 +0700

    Refactor `TypeOfMap`

--- packages/type-io-type/src/schema/base/TypeOfMap.ts
+++ packages/type-io-type/src/schema/base/TypeOfMap.ts
@@ -1,6 +1,6 @@
-import { UnknownSchema } from '.'
+import { AnySchema } from './AnySchema'
 import { TypeOf } from './TypeOf'
 
-export type TypeOfMap<T extends [...UnknownSchema[]]> = {
-  [K in keyof T]: T[K] extends UnknownSchema ? TypeOf<T[K]> : never
+export type TypeOfMap<T> = {
+  [K in keyof T]: T[K] extends AnySchema ? TypeOf<T[K]> : never
 }
