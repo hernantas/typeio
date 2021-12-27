014afccebf012b1f177049df8640dace36bafdff
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-12-27 14:28:31 +0700

    Add `parse` method to `TupleSchema`

--- packages/type-io-type/src/schema/tuple/TupleSchema.ts
+++ packages/type-io-type/src/schema/tuple/TupleSchema.ts
@@ -10,4 +10,19 @@ export class TupleSchema<T extends TupleSchemaType> extends BaseSchema<TypeOfMap
   get items (): T {
     return this.definition.items
   }
+
+  parse (input: unknown): TypeOfMap<T> {
+    if (!Array.isArray(input)) {
+      throw new Error('Input is not an array')
+    }
+
+    if (input.length !== this.definition.items.length) {
+      throw new Error(`Input array do not have "${this.definition.items.length}" length`)
+    }
+
+    return this
+      .definition
+      .items
+      .map((schema, index) => schema.parse(input[index])) as TypeOfMap<T>
+  }
 }
