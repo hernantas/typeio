ffed80eefa0a5992979913ecd66cf5703b626627
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-12-27 12:35:09 +0700

    Add `parse` to `ObjectSchema`

--- packages/type-io-type/src/schema/object/ObjectSchema.ts
+++ packages/type-io-type/src/schema/object/ObjectSchema.ts
@@ -1,3 +1,4 @@
+import { ObjectPropertyMap } from '.'
 import { ObjectType } from '../../alias'
 import { BaseSchema, SchemaMap } from '../base'
 import { ObjectSchemaDefinition } from './ObjectSchemaDefinition'
@@ -9,5 +10,19 @@ export class ObjectSchema<T extends ObjectType> extends BaseSchema<T, ObjectSche
 
   get properties (): SchemaMap<T> {
     return this.definition.properties
+  }
+
+  parse (input: unknown): T {
+    if (typeof input !== 'object') {
+      throw new Error('Input type is not an object')
+    }
+
+    const result: Partial<T> = {}
+    Object.keys(this.definition.properties).forEach(key => {
+      const tKey = key as keyof T
+      const value = (input as ObjectPropertyMap<T>)[tKey]
+      result[tKey] = this.definition.properties[tKey].parse(value)
+    })
+    return result as T
   }
 }
