72998b1e7c176f77b38d313aa54811110c681f76
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-12-27 12:27:54 +0700

    Add `parse` method to `LiteralSchema`

--- packages/type-io-type/src/schema/literal/LiteralSchema.ts
+++ packages/type-io-type/src/schema/literal/LiteralSchema.ts
@@ -6,4 +6,11 @@ export class LiteralSchema<T extends LiteralType> extends BaseSchema<T, LiteralS
   static create <T extends LiteralType> (value: T): LiteralSchema<T> {
     return new LiteralSchema({ value })
   }
+
+  parse (input: unknown): T {
+    if (input !== this.definition.value) {
+      throw new Error(`Literal value is not equal "${this.definition.value.toString()}"`)
+    }
+    return this.definition.value
+  }
 }
