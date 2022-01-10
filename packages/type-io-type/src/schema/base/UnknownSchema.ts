5e7ace40aa5adb85e9d907527bff41994516399f
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-10 12:38:37 +0700

    Implement `parse` method on `UnknownSchema`

--- packages/type-io-type/src/schema/base/UnknownSchema.ts
+++ packages/type-io-type/src/schema/base/UnknownSchema.ts
@@ -4,4 +4,8 @@ export class UnknownSchema extends BaseSchema<unknown> {
   static create (): UnknownSchema {
     return new UnknownSchema({})
   }
+
+  parse (input: unknown): unknown {
+    return input
+  }
 }
