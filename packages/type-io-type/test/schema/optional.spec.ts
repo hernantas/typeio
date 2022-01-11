f50bfdf94690c0db075faff65871017fe1aa1fa7
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-11 11:58:57 +0700

    Add `OptionalSchema` test

---
+++ packages/type-io-type/test/schema/optional.spec.ts
@@ -0,0 +1,18 @@
+import { OptionalSchema, StringSchema } from '../../src'
+import * as t from './shared'
+
+describe('Schema: StringSchema', () => {
+  const schema = OptionalSchema.create(StringSchema.create())
+
+  t.testArray(schema).invalid()
+  t.testBoolean(schema).invalid()
+  t.testLiteralString(schema).valid()
+  t.testLiteralNumber(schema).invalid()
+  t.testLiteralBoolean(schema).invalid()
+  t.testNull(schema).invalid()
+  t.testNumber(schema).invalid()
+  t.testObject(schema).invalid()
+  t.testDeepObject(schema).invalid()
+  t.testString(schema).valid()
+  t.testUndefined(schema).valid()
+})
