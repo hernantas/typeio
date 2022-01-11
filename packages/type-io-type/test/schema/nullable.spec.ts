bf246c458b2c5d0f1e0778f566d85fac3065d365
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-11 11:21:58 +0700

    Add `NullableSchema` test

---
+++ packages/type-io-type/test/schema/nullable.spec.ts
@@ -0,0 +1,16 @@
+import { NullableSchema, StringSchema } from '../../src'
+import * as t from './shared'
+
+describe('Schema: NullSchema', () => {
+  const schema = NullableSchema.create(StringSchema.create())
+
+  t.testArray(schema).invalid()
+  t.testBoolean(schema).invalid()
+  t.testLiteralString(schema).valid()
+  t.testLiteralNumber(schema).invalid()
+  t.testLiteralBoolean(schema).invalid()
+  t.testNull(schema).valid()
+  t.testNumber(schema).invalid()
+  t.testString(schema).valid()
+  t.testUndefined(schema).invalid()
+})
