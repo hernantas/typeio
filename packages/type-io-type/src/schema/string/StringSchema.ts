de3c4a1d53755ce9b75f693e400dc242802303b7
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-12 16:30:28 +0700

    Add string validation

--- packages/type-io-type/src/schema/string/StringSchema.ts
+++ packages/type-io-type/src/schema/string/StringSchema.ts
@@ -8,4 +8,24 @@ export class StringSchema extends BaseSchema<string> {
   is (input: unknown): input is string {
     return typeof input === 'string'
   }
+
+  min (value: number): this {
+    return this.addRule(v => v.length >= value, { kind: 'STRING_MIN' })
+  }
+
+  max (value: number): this {
+    return this.addRule(v => v.length <= value, { kind: 'STRING_MAX' })
+  }
+
+  length (value: number): this {
+    return this.addRule(v => v.length === value, { kind: 'STRING_LENGTH' })
+  }
+
+  pattern (pattern: RegExp): this {
+    return this.addRule(v => pattern.test(v), { kind: 'STRING_PATTERN' })
+  }
+
+  alphanumeric (): this {
+    return this.pattern(/^[a-zA-Z0-9]+$/)
+  }
 }
--- packages/type-io-type/test/schema/string.spec.ts
+++ packages/type-io-type/test/schema/string.spec.ts
@@ -1,3 +1,4 @@
+import { expect } from 'chai'
 import { StringSchema } from '../../src'
 import * as t from './shared'
 
@@ -15,4 +16,61 @@ describe('Schema: StringSchema', () => {
   t.testDeepObject(schema).invalid()
   t.testString(schema).valid()
   t.testUndefined(schema).invalid()
+
+  it('Validation: Min', () => {
+    const validator = schema.min(3)
+    expect(validator.validate('username')).to.have.length(0)
+    expect(validator.validate('usernam')).to.have.length(0)
+    expect(validator.validate('userna')).to.have.length(0)
+    expect(validator.validate('usern')).to.have.length(0)
+    expect(validator.validate('user')).to.have.length(0)
+    expect(validator.validate('use')).to.have.length(0)
+    expect(validator.validate('us')).to.have.length.greaterThan(0)
+    expect(validator.validate('u')).to.have.length.greaterThan(0)
+    expect(validator.validate('')).to.have.length.greaterThan(0)
+  })
+
+  it('Validation: Max', () => {
+    const validator = schema.max(3)
+    expect(validator.validate('username')).to.have.length.greaterThan(0)
+    expect(validator.validate('usernam')).to.have.length.greaterThan(0)
+    expect(validator.validate('userna')).to.have.length.greaterThan(0)
+    expect(validator.validate('usern')).to.have.length.greaterThan(0)
+    expect(validator.validate('user')).to.have.length.greaterThan(0)
+    expect(validator.validate('use')).to.have.length(0)
+    expect(validator.validate('us')).to.have.length(0)
+    expect(validator.validate('u')).to.have.length(0)
+    expect(validator.validate('')).to.have.length(0)
+  })
+
+  it('Validation: Length', () => {
+    const validator = schema.length(3)
+    expect(validator.validate('username')).to.have.length.greaterThan(0)
+    expect(validator.validate('usernam')).to.have.length.greaterThan(0)
+    expect(validator.validate('userna')).to.have.length.greaterThan(0)
+    expect(validator.validate('usern')).to.have.length.greaterThan(0)
+    expect(validator.validate('user')).to.have.length.greaterThan(0)
+    expect(validator.validate('use')).to.have.length(0)
+    expect(validator.validate('us')).to.have.length.greaterThan(0)
+    expect(validator.validate('u')).to.have.length.greaterThan(0)
+    expect(validator.validate('')).to.have.length.greaterThan(0)
+  })
+
+  it('Validation: Pattern', () => {
+    const validator = schema.pattern(/^[a-zA-Z0-9]+$/)
+    expect(validator.validate('UserName98543')).to.have.length(0)
+    expect(validator.validate('65891238912')).to.have.length(0)
+    expect(validator.validate('')).to.have.length.greaterThan(0)
+    expect(validator.validate('email@email')).to.have.length.greaterThan(0)
+    expect(validator.validate('user_name')).to.have.length.greaterThan(0)
+  })
+
+  it('Validation: Alphanumeric', () => {
+    const validator = schema.alphanumeric()
+    expect(validator.validate('UserName98543')).to.have.length(0)
+    expect(validator.validate('65891238912')).to.have.length(0)
+    expect(validator.validate('')).to.have.length.greaterThan(0)
+    expect(validator.validate('email@email')).to.have.length.greaterThan(0)
+    expect(validator.validate('user_name')).to.have.length.greaterThan(0)
+  })
 })
