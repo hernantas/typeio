d2ae2b4bed008107bfea8b9ce36d4abbfa28eee3
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-12 13:56:42 +0700

    Add number validation

--- packages/type-io-type/src/schema/number/NumberSchema.ts
+++ packages/type-io-type/src/schema/number/NumberSchema.ts
@@ -8,4 +8,28 @@ export class NumberSchema extends BaseSchema<number> {
   is (input: unknown): input is number {
     return typeof input === 'number'
   }
+
+  min (value: number): this {
+    return this.check(v => v >= value, { kind: 'number_min' })
+  }
+
+  max (value: number): this {
+    return this.check(v => v <= value, { kind: 'number_max' })
+  }
+
+  greater (value: number): this {
+    return this.check(v => v > value, { kind: 'number_greater' })
+  }
+
+  less (value: number): this {
+    return this.check(v => v < value, { kind: 'number_less' })
+  }
+
+  positive (): this {
+    return this.check(v => v > 0, { kind: 'number_positive' })
+  }
+
+  negative (): this {
+    return this.check(v => v < 0, { kind: 'number_negative' })
+  }
 }
