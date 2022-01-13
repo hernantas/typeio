10b907ae51d79af6541c4add37533c934b044908
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-13 10:39:47 +0700

    Add `type` readonly prop

--- packages/type-io-type/src/parser/codec/Codec.ts
+++ packages/type-io-type/src/parser/codec/Codec.ts
@@ -1,6 +1,11 @@
+import { ConstructorType } from '../../alias/ConstructorType'
+import { BaseSchema } from '../../schema/base/BaseSchema'
 import { Decoder } from './Decoder'
 import { Encoder } from './Encoder'
 
 export interface Codec<T, O = T, I = unknown> extends Decoder<T, I>, Encoder<T, O> {
-
+  /**
+   * Supported schema type
+   */
+  readonly type: ConstructorType<BaseSchema<T>>
 }
