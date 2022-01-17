f59ba28eee8826393481f8f21cc6a73f381d247a
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-17 10:38:37 +0700

    Add `NumberCodec`

---
+++ packages/type-io-type/src/parser/plain/NumberCodec.ts
@@ -0,0 +1,25 @@
+import { NumberSchema } from '../../schema/primitive/NumberSchema'
+import { Codec } from '../codec/Codec'
+
+export class NumberCodec implements Codec<number> {
+  readonly type = NumberSchema
+
+  readonly base = NumberSchema.create()
+
+  decode (value: unknown): number {
+    if (this.base.is(value)) {
+      return value
+    }
+
+    const result = Number(value)
+    if (Number.isNaN(result)) {
+      return result
+    }
+
+    throw new Error('Input type cannot be parsed into "Number"')
+  }
+
+  encode (value: number): number {
+    return value
+  }
+}
