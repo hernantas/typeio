d00dede20f6afde0b458fddb9146c7e5e8e538ec
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-18 11:39:54 +0700

    Change `Codec` generic `<T>` to `unknown` as default

--- packages/type-io-type/src/parser/codec/Codec.ts
+++ packages/type-io-type/src/parser/codec/Codec.ts
@@ -3,7 +3,7 @@ import { BaseSchema } from '../../schema/base/BaseSchema'
 import { Decoder } from './Decoder'
 import { Encoder } from './Encoder'
 
-export interface Codec<T, O = T, I = unknown> extends Decoder<T, I>, Encoder<T, O> {
+export interface Codec<T = unknown, O = T, I = unknown> extends Decoder<T, I>, Encoder<T, O> {
   /**
    * Supported schema type
    */
