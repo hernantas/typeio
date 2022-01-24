987de2b530b321c321b424cc102c862d98c403ab
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-24 12:33:19 +0700

    Add `ArrayCodec`

---
+++ packages/type-io-type/src/parser/codec/type/ArrayCodec.ts
@@ -0,0 +1,26 @@
+import { ArraySchema } from '../../../schema/array/ArraySchema'
+import { TypeOf } from '../../../schema/base/TypeOf'
+import { AnyCodec } from '../AnyCodec'
+import { Codec } from '../Codec'
+
+export class ArrayCodec<T extends AnyCodec> implements Codec<ArraySchema<T['schema']>> {
+  readonly schema: ArraySchema<T['schema']>
+
+  readonly codec: T
+
+  constructor (codec: T) {
+    this.codec = codec
+    this.schema = ArraySchema.create(codec.schema)
+  }
+
+  decode (value: unknown): Array<TypeOf<T['schema']>> {
+    if (Array.isArray(value)) {
+      return value.map(v => this.codec.decode(v))
+    }
+    throw new Error(`Input type cannot be parsed into "${this.schema.name}" type`)
+  }
+
+  encode (value: Array<TypeOf<T['schema']>>): Array<TypeOf<T['schema']>> {
+    return value.map(v => this.codec.encode(v))
+  }
+}
---
+++ packages/type-io-type/test/parser/codec/array.spec.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+import { ArrayCodec, StringCodec } from '../../../src'
+
+describe('Codec', () => {
+  describe('BooleanCodec', () => {
+    const codec = new ArrayCodec(new StringCodec())
+
+    it('Decode', () => {
+      expect(codec.decode([])).to.be.deep.equal([])
+      expect(codec.decode(['First', 'Second', 'Third'])).to.be.deep.equal(['First', 'Second', 'Third'])
+      expect(codec.decode([0, 1, 2, 3])).to.be.deep.equal(['0', '1', '2', '3'])
+    })
+
+    it('Encode', () => {
+      expect(codec.encode([])).to.be.deep.equal([])
+      expect(codec.encode(['First', 'Second', 'Third'])).to.be.deep.equal(['First', 'Second', 'Third'])
+    })
+  })
+})
