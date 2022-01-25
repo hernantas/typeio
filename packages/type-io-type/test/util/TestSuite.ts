b63e442bf23a023ae57cf180a4bedd73213045dd
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-25 17:47:38 +0700

    Add `TestSuite`

--- packages/type-io-type/test/util/TestSuite.ts
+++ packages/type-io-type/test/util/TestSuite.ts
@@ -1,4 +1,38 @@
-export interface TestSuite {
-  valid: () => void
-  invalid: () => void
+import { TestSuiteCase } from './TestSuiteCase'
+import { TransformFn } from './TransformFn'
+
+export class TestSuite<T> {
+  readonly cases: Array<TestSuiteCase<T>>
+
+  constructor (
+    readonly label: string,
+    transformer: TransformFn<T, any>,
+    values: T[]
+  ) {
+    this.cases = values.map(v => new TestSuiteCase(transformer, v))
+  }
+
+  each (fn: (testCase: TestSuiteCase<T>) => void): void {
+    it(this.label, () => this.cases.forEach(c => fn(c)))
+  }
+
+  is (value: unknown): void {
+    this.each(c => c.is(value))
+  }
+
+  isTrue (): void {
+    this.each(c => c.isTrue())
+  }
+
+  isFalse (): void {
+    this.each(c => c.isFalse())
+  }
+
+  isEqual (): void {
+    this.each(c => c.isEqual())
+  }
+
+  isThrow (): void {
+    this.each(c => c.isThrow())
+  }
 }
