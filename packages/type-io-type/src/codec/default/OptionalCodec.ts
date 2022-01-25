56231f3db1d7296c59709886b84549735f753918
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-01-25 20:14:47 +0700

    Add `OptionalCodec`

---
+++ packages/type-io-type/src/codec/default/OptionalCodec.ts
@@ -0,0 +1,23 @@
+import { TypeOf } from '../../schema/helper/TypeOf'
+import { OptionalSchema } from '../../schema/OptionalSchema'
+import { AnyCodec } from '../AnyCodec'
+import { Codec } from '../Codec'
+
+export class OptionalCodec<T extends AnyCodec> implements Codec<OptionalSchema<T['schema']>> {
+  readonly schema: OptionalSchema<T['schema']>
+
+  readonly codec: T
+
+  constructor (codec: T) {
+    this.codec = codec
+    this.schema = OptionalSchema.create(codec.schema)
+  }
+
+  decode (value: unknown): TypeOf<T['schema']> | undefined {
+    return value === undefined ? undefined : this.codec.decode(value)
+  }
+
+  encode (value: TypeOf<T['schema']> | undefined): TypeOf<T['schema']> | undefined {
+    return this.codec.encode(value)
+  }
+}
