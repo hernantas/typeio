3f868252c1210794dc622698ab330ed69ea57476
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-02-01 09:01:00 +0700

    Add generic with default to `AnyCodec`

--- packages/type-io-type/src/codec/default/AnyCodec.ts
+++ packages/type-io-type/src/codec/default/AnyCodec.ts
@@ -2,4 +2,4 @@ import { AnySchema } from '../../schema/AnySchema'
 import { Codec } from '../Codec'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type AnyCodec = Codec<AnySchema, any, any>
+export type AnyCodec<T extends AnySchema = AnySchema> = Codec<T, any, any>
