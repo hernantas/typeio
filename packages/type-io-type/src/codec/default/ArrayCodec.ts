31a37062e91901ad16cd20ea5e195ac464007d4c
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-02-03 10:06:40 +0700

    Change `ArrayCodec` to accept base type

--- packages/type-io-type/src/codec/default/ArrayCodec.ts
+++ packages/type-io-type/src/codec/default/ArrayCodec.ts
@@ -1,7 +1,6 @@
 import { ArraySchema } from '../../schema/ArraySchema'
 import { TypeOf } from '../../schema/helper/TypeOf'
 import { Codec } from '../Codec'
-import { DecodeError } from '../error/DecodeError'
 import { InputOf } from '../helper/InputOf'
 import { OutputOf } from '../helper/OutputOf'
 import { SchemaOf } from '../helper/SchemaOf'
@@ -9,7 +8,7 @@ import { AnyCodec } from './AnyCodec'
 
 export class ArrayCodec<T extends AnyCodec>
   implements
-    Codec<ArraySchema<SchemaOf<T>>, OutputOf<T>[], InputOf<T>[] | unknown>
+    Codec<ArraySchema<SchemaOf<T>>, OutputOf<T>[], InputOf<T>[] | InputOf<T>>
 {
   readonly schema: ArraySchema<SchemaOf<T>>
 
@@ -20,13 +19,14 @@ export class ArrayCodec<T extends AnyCodec>
     this.schema = ArraySchema.create(codec.schema)
   }
 
-  decode(value: InputOf<T>[] | unknown): TypeOf<SchemaOf<T>>[] {
+  decode(value: InputOf<T>[] | InputOf<T>): TypeOf<SchemaOf<T>>[] {
     if (Array.isArray(value)) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return value.map((v) => this.codec.decode(v))
     }
 
-    throw new DecodeError(this.schema.name)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return [this.codec.decode(value)]
   }
 
   encode(value: TypeOf<SchemaOf<T>>[]): OutputOf<T>[] {
