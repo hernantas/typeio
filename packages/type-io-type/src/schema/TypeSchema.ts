2e408d6689baa575d448d34d402635fcfa083b4c
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-02-20 11:49:00 +0700

    Add `properties` prop to `TypeSchemaDefinition`

--- packages/type-io-type/src/schema/TypeSchema.ts
+++ packages/type-io-type/src/schema/TypeSchema.ts
@@ -1,16 +1,26 @@
-import { BaseSchema } from './BaseSchema'
 import { ConstructorType } from '../alias/ConstructorType'
+import { getMetadata } from '../decorator/metadata'
+import { BaseSchema } from './BaseSchema'
 import { TypeDefinition } from './definition/TypeDefinition'
+import { SchemaMap } from './helper/SchemaMap'
 
 export class TypeSchema<T> extends BaseSchema<T, TypeDefinition<T>> {
   readonly _kind: string = 'type'
 
   static create<T>(constructor: ConstructorType<T>): TypeSchema<T> {
-    return new TypeSchema({ name: constructor.name, type: constructor })
+    return new TypeSchema({
+      name: constructor.name,
+      type: constructor,
+      properties: getMetadata(constructor),
+    })
   }
 
   get type(): ConstructorType<T> {
     return this.definition.type
+  }
+
+  get properties(): Partial<SchemaMap<T>> {
+    return this.definition.properties
   }
 
   override is(input: unknown): input is T {
--- packages/type-io-type/src/schema/definition/TypeDefinition.ts
+++ packages/type-io-type/src/schema/definition/TypeDefinition.ts
@@ -1,6 +1,9 @@
 import { ConstructorType } from '../../alias/ConstructorType'
+import { SchemaMap } from '../helper/SchemaMap'
 import { BaseDefinition } from './BaseDefinition'
 
 export interface TypeDefinition<T> extends BaseDefinition {
   readonly type: ConstructorType<T>
+
+  readonly properties: Partial<SchemaMap<T>>
 }
