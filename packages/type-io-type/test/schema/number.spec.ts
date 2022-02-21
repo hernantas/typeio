8b4d38de60c04af4c5186236d52289920096577e
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-02-21 08:31:30 +0700

    Add `Validation` wrapper test

--- packages/type-io-type/test/schema/number.spec.ts
+++ packages/type-io-type/test/schema/number.spec.ts
@@ -27,47 +27,49 @@ describe('Schema: NumberSchema', () => {
     suite.undefined.isFalse()
   })
 
-  it('Validation: Min', () => {
-    const validator = schema.min(0)
-    expect(validator.validate(0)).to.have.length(0)
-    expect(validator.validate(1)).to.have.length(0)
-    expect(validator.validate(Number.MAX_SAFE_INTEGER)).to.have.length(0)
-    expect(validator.validate(-1)).to.have.length.greaterThan(0)
-    expect(
-      validator.validate(Number.MIN_SAFE_INTEGER)
-    ).to.have.length.greaterThan(0)
-  })
+  describe('Validation', () => {
+    it('Min', () => {
+      const validator = schema.min(0)
+      expect(validator.validate(0)).to.have.length(0)
+      expect(validator.validate(1)).to.have.length(0)
+      expect(validator.validate(Number.MAX_SAFE_INTEGER)).to.have.length(0)
+      expect(validator.validate(-1)).to.have.length.greaterThan(0)
+      expect(
+        validator.validate(Number.MIN_SAFE_INTEGER)
+      ).to.have.length.greaterThan(0)
+    })
 
-  it('Validation: Max', () => {
-    const validator = schema.max(0)
-    expect(validator.validate(0)).to.have.length(0)
-    expect(validator.validate(-1)).to.have.length(0)
-    expect(validator.validate(Number.MIN_SAFE_INTEGER)).to.have.length(0)
-    expect(validator.validate(1)).to.have.length.greaterThan(0)
-    expect(
-      validator.validate(Number.MAX_SAFE_INTEGER)
-    ).to.have.length.greaterThan(0)
-  })
+    it('Max', () => {
+      const validator = schema.max(0)
+      expect(validator.validate(0)).to.have.length(0)
+      expect(validator.validate(-1)).to.have.length(0)
+      expect(validator.validate(Number.MIN_SAFE_INTEGER)).to.have.length(0)
+      expect(validator.validate(1)).to.have.length.greaterThan(0)
+      expect(
+        validator.validate(Number.MAX_SAFE_INTEGER)
+      ).to.have.length.greaterThan(0)
+    })
 
-  it('Validation: Greater', () => {
-    const validator = schema.greater(0)
-    expect(validator.validate(1)).to.have.length(0)
-    expect(validator.validate(Number.MAX_SAFE_INTEGER)).to.have.length(0)
-    expect(validator.validate(0)).to.have.length.greaterThan(0)
-    expect(validator.validate(-1)).to.have.length.greaterThan(0)
-    expect(
-      validator.validate(Number.MIN_SAFE_INTEGER)
-    ).to.have.length.greaterThan(0)
-  })
+    it('Greater', () => {
+      const validator = schema.greater(0)
+      expect(validator.validate(1)).to.have.length(0)
+      expect(validator.validate(Number.MAX_SAFE_INTEGER)).to.have.length(0)
+      expect(validator.validate(0)).to.have.length.greaterThan(0)
+      expect(validator.validate(-1)).to.have.length.greaterThan(0)
+      expect(
+        validator.validate(Number.MIN_SAFE_INTEGER)
+      ).to.have.length.greaterThan(0)
+    })
 
-  it('Validation: Less', () => {
-    const validator = schema.less(0)
-    expect(validator.validate(-1)).to.have.length(0)
-    expect(validator.validate(Number.MIN_SAFE_INTEGER)).to.have.length(0)
-    expect(validator.validate(0)).to.have.length.greaterThan(0)
-    expect(validator.validate(1)).to.have.length.greaterThan(0)
-    expect(
-      validator.validate(Number.MAX_SAFE_INTEGER)
-    ).to.have.length.greaterThan(0)
+    it('Less', () => {
+      const validator = schema.less(0)
+      expect(validator.validate(-1)).to.have.length(0)
+      expect(validator.validate(Number.MIN_SAFE_INTEGER)).to.have.length(0)
+      expect(validator.validate(0)).to.have.length.greaterThan(0)
+      expect(validator.validate(1)).to.have.length.greaterThan(0)
+      expect(
+        validator.validate(Number.MAX_SAFE_INTEGER)
+      ).to.have.length.greaterThan(0)
+    })
   })
 })
