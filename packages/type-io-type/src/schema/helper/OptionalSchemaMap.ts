f0a14276c0cd866db5a0a5eb6cb82cf22d3acd7e
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-02-21 09:39:34 +0700

    Replace `ObjectType` with `ObjectSchemaType` on `OptionalSchemaMap`

--- packages/type-io-type/src/schema/helper/OptionalSchemaMap.ts
+++ packages/type-io-type/src/schema/helper/OptionalSchemaMap.ts
@@ -1,7 +1,6 @@
-import { ObjectType } from '../../alias/ObjectType'
-import { SchemaAny } from '../alias/SchemaAny'
+import { ObjectSchemaType } from '../alias/ObjectSchemaType'
 import { OptionalSchema } from '../OptionalSchema'
 
-export type OptionalSchemaMap<T extends ObjectType<SchemaAny>> = {
+export type OptionalSchemaMap<T extends ObjectSchemaType> = {
   [K in keyof T]: OptionalSchema<T[K]>
 }
