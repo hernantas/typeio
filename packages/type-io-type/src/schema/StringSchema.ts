58ba8a43fd0e35690d06bdbbcd315c6904c15e6e
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-03-09 14:13:01 +0700

    Add URL validation
    Add

--- packages/type-io-type/src/schema/StringSchema.ts
+++ packages/type-io-type/src/schema/StringSchema.ts
@@ -6,6 +6,9 @@ const regexEmail =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const regexUuid =
   /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/
+const regexUrl =
+  // eslint-disable-next-line no-useless-escape
+  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
 
 export class StringSchema extends BaseSchema<string> {
   readonly _kind: string = 'string'
@@ -78,6 +81,14 @@ export class StringSchema extends BaseSchema<string> {
       name: 'STRING_PATTERN_UUID',
       args: { pattern: regexUuid },
       validate: (v) => regexUuid.test(v),
+    })
+  }
+
+  url(): this {
+    return this.check({
+      name: 'STRING_PATTERN_URL',
+      args: { pattern: regexUrl },
+      validate: (v) => regexUrl.test(v),
     })
   }
 }
